061b3829b45de38b4b986843b34f75599a2ae9ae
proshantosaha <proshantoll71@gmail.com>
2024-12-12 19:48:41 +0600

    get all post query completed

--- queries/blog-data.js
+++ queries/blog-data.js
@@ -4,40 +4,43 @@ import { gql } from "graphql-request";
 export const getAllPosts = async (tags) => {
   const client = getClient();
 
-  const data = await client.request(gql`
-    query allPosts($tags: [ObjectId!]) {
-      publication(host: "blog.greenroots.info") {
-        title
-        posts(first: 20) {
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
-          edges {
-            node {
-              author {
-                name
-                profilePicture
-              }
-              title
-              subtitle
-              brief
-              slug
-              coverImage {
-                url
-              }
-              tags {
-                name
+  const data = await client.request(
+    gql`
+      query allPosts($tags: [ObjectId!]) {
+        publication(host: "blog.greenroots.info") {
+          title
+          posts(first: 20, filter: { tags: $tags }) {
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+            edges {
+              node {
+                author {
+                  name
+                  profilePicture
+                }
+                title
+                subtitle
+                brief
                 slug
-                id
+                coverImage {
+                  url
+                }
+                tags {
+                  name
+                  slug
+                  id
+                }
+                publishedAt
+                readTimeInMinutes
               }
-              publishedAt
-              readTimeInMinutes
             }
           }
         }
       }
-    }
-  `);
-  return data;
+    `,
+    { tags: tags }
+  );
+  return data.publication.posts.edges;
 };
