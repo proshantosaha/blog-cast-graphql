6d424ba1a76aefd1add0b1b384089a0de1faecb7
proshantosaha <proshantoll71@gmail.com>
2024-12-21 23:09:34 +0600

    image url set next.config file

--- app/components/posts/latest-post.jsx
+++ app/components/posts/latest-post.jsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const LatestPost = ({ post }) => {
   return (
     <article className="mt-4">
-      <h3>{post?.title}</h3>
-      <p>posted on {post?.publishedAt}</p>
+      <h3 className="text-xl font-semibold">{post?.title}</h3>
+      <p className="text-gray-500">posted on {post?.publishedAt}</p>
+      <Image
+        className="mt-4 object-cover rounded-md xs:w-full sm:w-full md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:2-1/3"
+        width={500}
+        height={500}
+        alt={post?.title}
+        src={post?.coverImage?.url}
+      />
+
+      <p className="mt-4 text-gray-700">{post?.breif}</p>
     </article>
   );
 };
--- app/components/posts/post-list.jsx
+++ app/components/posts/post-list.jsx
@@ -11,20 +11,30 @@ const PostList = async () => {
   const restPosts = posts.slice(1);
   return (
     <>
-      <div className="px-8 py-8 bg-gray-100"><LatestPost post={latestPost}/></div>
+      <div className="px-8 py-8 bg-gray-100">
+        <LatestPost post={latestPost} />
+      </div>
 
-      {restPosts.length>0&&(
-       <div className="px-8 py-16"> <div className="grid gap-4 mt-4  grid-cols-1 md:grid-cols-2 lg:grid-cols-2">{restPosts.map((post)=>(
+      {restPosts.length > 0 && (
+        <div className="px-8 py-16">
+          {/* <div className="grid gap-4 mt-4  grid-cols-1 md:grid-cols-2 lg:grid-cols-2">{restPosts.map((post)=>(
           
-       ))}</div>
+       ))}
        
-       <button className="inline-flex items-center justify-center
+       
+       </div> */}
+
+          <button
+            className="inline-flex items-center justify-center
         rounded-md text-sm font-medium ring-offset-background 
         transition-colors focus-visible:outline-none focus-visible:ring-2 
         focus-visible:ring-ring focus-visible:ring-offset-2 
         disabled:pointer-events-none disabled:opacity-50
-         bg-black text-white hover:bg-black/90 h-10 px-4 py-2 mt-8">Show More Posts</button>
-       </div>
+         bg-black text-white hover:bg-black/90 h-10 px-4 py-2 mt-8"
+          >
+            Show More Posts
+          </button>
+        </div>
       )}
     </>
   );
